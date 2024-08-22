f8439b320940d9e9106c66ff44134f94abefecf2
Sun choi <csun2772@naver.com>
2024-08-22 20:25:52 +0900

    init commit

---
+++ src/main/java/com/sparta/jpascheduletask/JpaScheduleTaskApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.jpascheduletask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaScheduleTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaScheduleTaskApplication.class, args);
+    }
+
+}
